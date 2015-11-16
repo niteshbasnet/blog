936259b43a9e3fe2023cf8121534e01b1b2b149f
boldhuu <d.boldkhuu@gmail.com>
2015-11-16 11:12:50 -0600

    version upgraded and fixed

--- Blog/src/main/java/mum/edu/blog/controller/LoginController.java
+++ Blog/src/main/java/mum/edu/blog/controller/LoginController.java
@@ -1,19 +1,24 @@
 package mum.edu.blog.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import mum.edu.blog.domain.User;
 
 @Controller
 public class LoginController {
 
 	@RequestMapping(value="/login", method = RequestMethod.GET)
 	public String login() {
- 		return "login";
+		
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	    String name = auth.getName();
+	    
+	    if(name.equals("anonymousUser"))
+	    	return "login";
+	    else return "redirect:/";
 	}
  
 	@RequestMapping(value="/loginfailed", method = RequestMethod.GET)
@@ -24,13 +29,23 @@ public class LoginController {
  
 	}
  
-	@RequestMapping(value="/logout", method = RequestMethod.GET)
-	public String logout(Model model) {
- 		return "redirect:/";
- 	}
+//	@RequestMapping(value="/logout", method = RequestMethod.GET)
+//	public String logout(Model model) {
+// 		return "redirect:/";
+// 	}
 	
-	@RequestMapping(value="/signUp", method = RequestMethod.GET)
-	public String signup(@ModelAttribute("newUser") User user) {
- 		return "signUp";
- 	}
+//	@RequestMapping(value="/signUp", method = RequestMethod.GET)
+//	public String signup(@ModelAttribute("newUser") User user) {
+//		
+//		System.out.println("get method coming");
+// 		return "signUp";
+// 	}
+//	
+//	@RequestMapping(value="/signUp", method = RequestMethod.POST)
+//	public String userSave(@ModelAttribute("newUser") @Valid User user, BindingResult result) {
+// 		if(result.hasErrors()) return "signUp";
+// 		
+// 		System.out.println("post method coming");
+// 		return "home";
+// 	}
 }
