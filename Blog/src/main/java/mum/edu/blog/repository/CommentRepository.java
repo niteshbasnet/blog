d520c795de6cae0ab01362cd32586e34243e437a
Nitesh Basnet <naam.nitesh@gmail.com>
2015-11-16 22:22:48 -0600

    no message

--- Blog/src/main/java/mum/edu/blog/repository/CommentRepository.java
+++ Blog/src/main/java/mum/edu/blog/repository/CommentRepository.java
@@ -6,10 +6,14 @@ import mum.edu.blog.domain.Article;
 import mum.edu.blog.domain.Comment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-	Comment findCommentById(long Id);
+	public Comment findCommentById(long Id);
 
-	List<Comment> findCommentByArticle(Article article);
+	public List<Comment> findCommentByArticle(Article article);
+
+	@Query(value = "insert into Comment(COMMENT,ART_ID) values (?comment, ?articleId)", nativeQuery = true)
+	public void saveComment(String comment, long articleId);
 
 }
